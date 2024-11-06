e2cc121c68741d5856ff598ae28e126aa6f8cc85
jesperjesper <simpsekundaer@gmail.com>
2024-11-06 13:21:27 +0100

    task 3-3 done ish

--- DAT101-main/module_3/task_3-3/task_3-3.mjs
+++ DAT101-main/module_3/task_3-3/task_3-3.mjs
@@ -3,28 +3,105 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function todaynorway() {
+    // get todays date
+    const today = new Date();
+    // format the date
+    const norwegiandate = today.toLocaleDateString("no-NB");
+    // print the formatted date
+    printOut(norwegiandate);
+}
+todaynorway();
+
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("N/A");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function circle(radius)
+{
+
+    let diameter = radius + radius;
+    let circumferance = Math.PI * diameter;
+    let area = Math.PI * radius * radius;
+
+    printOut("your radius was: " + radius);
+    printOut("the diamter of your circle is: " + diameter);
+    printOut("The circumferance of this cirlce is: " + circumferance);
+    printOut("the area of this circle is: " + area);
+}
+circle(5); //the radius is set here
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function rectangledimensions(rectangle) {
+    let circumference = 2 * (rectangle.width + rectangle.height);
+    let area = rectangle.width * rectangle.height;
+
+    printOut("width is: " + rectangle.width);
+    printOut("height is: " + rectangle.height);
+    // Print the results
+    printOut("the circumference of the rectangle is: " + circumference);
+    printOut("the area of the rectangle is: " + area);
+
+}
+rectangledimensions({ width: 5, height: 5 });
 printOut(newLine);
 
+
+
+
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function conversion(temperature, type) {
+    let celsius, fahrenheit, kelvin;
+
+    if (type === "C") { //celcius conversions
+        celsius = temperature;
+        let KfromC = Math.round(celsius + 273.15);
+        let FfromC = Math.round(celsius * 9/5 + 32);
+        kelvin = KfromC;
+        fahrenheit = FfromC;
+    } 
+    else if (type === "K") { //kelvin conversions
+     
+        kelvin = temperature;
+        let FfromK = Math.round(1.8 * (kelvin - 273.15) + 32);
+        let CfromK = Math.round(kelvin - 273.15);
+        celsius = CfromK;
+        fahrenheit = FfromK;
+    } 
+    else if (type === "F") { //fahrenheit conversios
+        fahrenheit = temperature;
+        let KfromF = Math.round((fahrenheit - 32) * 5/9 + 273.15);
+        let CfromF = Math.round((fahrenheit - 32) * 5/9);
+        kelvin = KfromF;
+        celsius = CfromF;
+    } else {
+        return;
+    }
+
+    printOut("Original temperature: " + temperature + " " + type);
+    printOut("Celsius: " + celsius + "°C");
+    printOut("Kelvin: " + kelvin + "K");
+    printOut("Fahrenheit: " + fahrenheit + "°F");
+}
+
+conversion(15, "C"); 
+printOut(newLine);
+conversion(100, "F");  
+printOut(newLine);
+conversion(20, "K"); 
+
+
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
@@ -49,6 +126,21 @@ printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function factorial(n) {
+    // if n is 0 or 1, the factorial is 1
+    if (n <= 1) {
+        return 1;
+    } 
+    //use this function to find parameters of other numbers
+    return n * factorial(n - 1);
+}
+
+
+let number = 5;
+printOut("factorial of " + number + " is: " + factorial(number));
+
+
+
+
 printOut(newLine);
